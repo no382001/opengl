87b574ddbd5061396925022773f0b84e1dae0289
no392001 <102482527+no382001@users.noreply.github.com>
2022-07-03 23:43:22 +0200

    lesson2

--- udemy/lesson2/1main.cpp
+++ udemy/lesson2/1main.cpp
@@ -4,7 +4,112 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-const GLint WIDTH = 400, HEIGHT = 600;
+const GLint WIDTH = 400, HEIGHT = 400;
+
+GLuint VAO, VBO, shader;
+
+//vertex shader
+static const char* vShader = "                      \n\
+#version 330                                        \n\
+                                                    \n\
+layout (location = 0) in vec3 pos;                  \n\
+                                                    \n\
+void main()                                         \n\
+{                                                   \n\
+    gl_Position = vec4(0.4 * pos.x, 0.4 *pos.y,pos.z,1.0);      \n\
+}";
+
+static const char* fShader = "                      \n\
+#version 330                                        \n\
+                                                    \n\
+out vec4 colour;                  \n\
+                                                    \n\
+void main()                                         \n\
+{                                                   \n\
+    colour = vec4(1.0,0.0,0.0,1.0);      \n\
+}";
+
+
+void createTriangle(){
+    //triangle
+    //center is 0.0
+    GLfloat vertices[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    };
+
+    glGenVertexArrays(1,&VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1,&VBO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //put to location 0
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(0);
+}
+
+void addShader(GLuint theProgram, const char* shaderCode, GLenum shaderType){
+    
+    GLuint theShader = glCreateShader(shaderType);
+    const GLchar* theCode[1];
+    theCode[0] = shaderCode;
+
+    GLint codeLength[1];
+    codeLength[0] = strlen(shaderCode);
+
+    glShaderSource(theShader,1,theCode,codeLength);
+    glCompileShader(theShader);
+
+    GLint result = 0;
+    GLchar eLog[1024] = {0};
+
+    glGetShaderiv(theShader,GL_COMPILE_STATUS,&result);
+    if(!result){
+        glGetShaderInfoLog(theShader, sizeof(eLog), NULL, eLog);
+        printf("Error compiling the %d shader: '%s'\n",shaderType,eLog);
+        return;
+    }
+
+    glAttachShader(theProgram,theShader);
+
+}
+
+void compileShaders(){
+
+    shader = glCreateProgram();
+    if(!shader){
+        printf("shader program was not created");
+        return; //crash
+    }
+
+    addShader(shader,vShader,GL_VERTEX_SHADER);
+    addShader(shader,fShader,GL_FRAGMENT_SHADER);
+
+    GLint result = 0;
+    GLchar eLog[1024] = {0};
+
+    glLinkProgram(shader);
+    glGetProgramiv(shader,GL_LINK_STATUS, &result);
+    if(!result){
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error linking program: '%s'\n",eLog);
+        return;
+    }
+
+    glValidateProgram(shader);
+    glGetProgramiv(shader,GL_VALIDATE_STATUS, &result);
+    if(!result){
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error validating program: '%s'\n",eLog);
+        return;
+    }
+}
 
 int main(){
     //init glfw
@@ -53,6 +158,9 @@ int main(){
     //set viewport size
     glViewport(0,0,bufferWidth,bufferHeight);
 
+    createTriangle();
+    compileShaders();
+
     //loop until window closed
 
     while(!glfwWindowShouldClose(mainWindow)){
@@ -60,8 +168,14 @@ int main(){
         glfwPollEvents();
         
         //clear window (r,g,b,a)
-        glClearColor(1.0f,0.0f,0.0f,1.0f);
+        glClearColor(0.0f,0.0f,0.0f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+            glBindVertexArray(VAO);
+            glDrawArrays(GL_TRIANGLES, 0,3);
+            glBindVertexArray(0);
+        glUseProgram(0);
 
         glfwSwapBuffers(mainWindow);
 
